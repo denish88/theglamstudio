7d7666d7ab91e232a0393a4a9222893e91166594
denish88 <143495130+denish88@users.noreply.github.com>
2026-07-18 20:22:20 +0530

    chnage password method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,7 +9,7 @@ const {
 } = require('../utils/memberKeyId')
 
 function generatePassword() {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const chars = 'ABCDEFGHJKMNPQRSTUVWXYZ23456789'
   let password = ''
   for (let i = 0; i < 8; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length))
